3aff30f9c14499955f7377f1f9ca25286fa22931
JTCHE <john.chedeville@gmail.com>
2026-08-03 16:19:28 +0200

    feat(copy): adjust homepage copywriting

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
       <div className="px-6 max-w-4xl mx-auto w-full">
         <h1 className="text-3xl font-bold tracking-tight leading-6">HoudiniMD</h1>
         <p className="text-muted-foreground mt-2 mb-5">
-          Blazing fast, LLM-optimized documentation for SideFX Houdini. <br />
-          Type a node name, VEX function, or paste a SideFX URL, and get clean markdown.
+          Blazing-fast, Markdown mirror of the Houdini docs <br />
+          Type a node name, a VEX function, or paste a SideFX URL
         </p>
         <HomeSearchField />
       </div>
